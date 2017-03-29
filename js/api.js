4410a3644922932724d03503edd52a688485cc7e
imudges00lipeiran <lipeiran@imudges.com>
2017-03-29 21:30:26 +0800

    3.29 21:30

--- js/api.js
+++ js/api.js
@@ -4,12 +4,16 @@
 $(function () {
     $('#phone').click(function () {
         var phoneNumber=$("#txt").val();
+
         $.ajax({
             type: "GET",
-            url: "http://apis.juhe.cn/mobile/get?phone="+phoneNumber+"&key=21996f07fa43b5f1cf44a2886f300f15",
-            dataType: "jsonp",
-            jsonp: "callback",
-            jsonpCallback: "doFunction",
+            url: "http://localhost:3000",
+            data: {
+                address: "apis.juhe.cn/mobile/get?phone="+phoneNumber+"&key=21996f07fa43b5f1cf44a2886f300f15"
+            },
+            dataType: "json",
+            // jsonp: "callback",
+            // jsonpCallback: "doFunction",
             error: function() {
                 alert("Connection error");
             },
@@ -33,10 +37,13 @@ $(function () {
         var postal=$("#postal").val();
         $.ajax({
             type: "GET",
-            url: "http://v.juhe.cn/postcode/query?postcode="+postal+"&key=578bb5fc58bf3177b8c50ec885e24e45",
-            dataType: "jsonp",
-            jsonp: "callback",
-            jsonpCallback: "doFunction",
+            url: "http://localhost:3000",
+            data:{
+                address:"v.juhe.cn/postcode/query?postcode="+postal+"&key=578bb5fc58bf3177b8c50ec885e24e45"
+            },
+            dataType: "json",
+            // jsonp: "callback",
+            // jsonpCallback: "doFunction",
             error: function() {
                 alert("Connection error");
             },
@@ -47,12 +54,67 @@ $(function () {
         })
     });
     function showData(data){
+        var result=data.result.list;
+        for (var i=1;i<=5;i++)
+        {
+        $("#paragraph").html(
+            "邮编:" + result[i].PostNumber +
+            "<br/>省份：" + result[i].Province +
+            "<br/>城市：" + result[i].City +
+            "<br/>区：" + result[i].District +
+            "<br/>地址：" + result[i].Address
+
+        )}
+        }
+});
+$(function () {
+    var url1="web.juhe.cn:8080/finance/gold/shgold";
+    var key;
+    $('#option1').click(function () {
+        url1="web.juhe.cn:8080/finance/gold/shgold";
+    });
+    $('#option2').click(function () {
+        url1="web.juhe.cn:8080/finance/gold/shfuture";
+    });
+    $('#option3').click(function () {
+        url1="web.juhe.cn:8080/finance/gold/bankgold";
+    });
+    $('#golden').click(function () {
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:3000",
+            data: {
+              address:url1+"?v=&key=8cc9e642ba492d27c0dac28bdb64da8a"
+            },
+            dataType: "json",
+            // jsonp: "callback",
+            // jsonpCallback: "doFunction",
+            error:function() {
+                alert("Connection error");
+            },
+            success:function(data){
+                alert("Connection success")
+                showData(data);
+            }
+        })
+    });
+    function showData(data){
         var result=data.result;
         $("#paragraph").html(
-            "邮编:"+result.PostNumber+
-            "<br/>省份："+result.Province+
-            "<br/>城市："+result.City+
-            "<br/>区："+result.District+
-            "<br/>地址："+result.Address
+            "品种:"+result.variety+
+            "<br/>最新价："+result.latestpri+
+            "<br/>开盘价："+result.openpri+
+            "<br/>最高价："+result.maxpri+
+            "<br/>最低价："+result.minpri+
+            "<br/>更新时间："+result.time
         )}
-});
+})
+// "variety":"Ag(T+D)",		/*品种*/
+//     "latestpri":"6585.00",		/*最新价*/
+//     "openpri":"6712.00",		/*开盘价*/
+//     "maxpri":"6721.00",		/*最高价*/
+//     "minpri":"6581.00",		/*最低价*/
+//     "limit":"-1.98%",		/*涨跌幅*/
+//     "yespri":"6718.00",		/*昨收价*/
+//     "totalvol":"1564524.0000",	/*总成交量*/
+//     "time":"2012-12-19 15:29:59"	/*更新时间*/
